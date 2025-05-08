8df98e9d5fff37dfe1b8b5012cc899aaa2b428d2
abangdsm <id.reversal@gmail.com>
2025-05-08 09:29:59 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Github Remote</title>
+</head>
+<body>
+    <h1>Halo programmers</h1>
+</body>
+</html>
